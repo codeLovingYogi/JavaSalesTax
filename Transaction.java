c76a990fbc7b3b57f373555bb23e86e6ed69fa06
Felice H <yogilovingcode@gmail.com>
2016-11-13 13:41:34 -0500

    Add Product and Tests classes

--- Transaction.java
+++ Transaction.java
@@ -6,26 +6,27 @@ import java.util.Arrays;
 import java.math.BigDecimal;
 
 public class Transaction {
-	public ArrayList<String> scanItems(String file) throws FileNotFoundException {
+	public static ArrayList<String> scanItems(String file) throws FileNotFoundException {
 		ArrayList<String> scannedItems = new ArrayList<String>();
 		Scanner scan = new Scanner(new File(file)).useDelimiter("\\n");
 		while(scan.hasNext()) {
-			//System.out.println(scan.next());
 			scannedItems.add(scan.next());
 		}
 		scan.close();
-		// System.out.println(scannedItems);
 		return(scannedItems);
 	}
 
 	public void getProductInfo(ArrayList<String> items) {
 		for(String item: items) {
 			System.out.println(item);
-			Boolean imported = false;
+			int quantity;
+			boolean imported = false;
+			boolean exempt = false;
 			ArrayList<String> getProductName = new ArrayList<String>();
 			BigDecimal price = null;
+
 			String[] info = item.split("\\s+");
-			int quantity = Integer.parseInt(info[0]);
+			quantity = Integer.parseInt(info[0]);
 			// System.out.println(Arrays.toString(info));
 			for (int i = 1; i < info.length; i++) {
 				//System.out.println(info[i]);
@@ -45,13 +46,84 @@ public class Transaction {
 			System.out.println(imported);
 			System.out.println(price);
 
+			Product product = new Product(name, price, exempt, imported);
 		}
 	}
 	public static void main (String[] args) throws FileNotFoundException {
-		String file1 = "Input2.txt";
+		Tests.checkScanItems();
+		Tests.checkProduct();
+		// String file = "Input1.txt";
+		String file = "Input2.txt";
+		// String file = "Input3.txt";
 		Transaction t1 = new Transaction();
-		ArrayList<String> items = t1.scanItems(file1);
+		ArrayList<String> items = t1.scanItems(file);
 		//System.out.println(items);
 		t1.getProductInfo(items);
+	}
+}
+
+class Product {
+	private String name;
+	private BigDecimal price;;
+	private boolean exempt;
+	private boolean imported;
+
+	public Product(String name, BigDecimal price, boolean exempt, boolean imported) {
+		this.name = name;
+		this.price = price;
+		this.exempt = exempt;
+		this.imported = imported;
+		// System.out.println("A product has been created");
+	}
+
+	public String name() {
+		return this.name;
+	}
+
+	public BigDecimal price() {
+		return this.price;
+	}
+}
+
+class Tests {
+	public static void checkScanItems () throws FileNotFoundException {
+		ArrayList<String> expectedItems;
+		int expectedInt;
+		// Contents of Input3.txt:
+		// 1 imported bottle of perfume at 27.99
+		// 1 bottle of perfume at 18.99
+		// 1 packet of headache pills at 9.75
+		// 1 box of imported chocolates at 11.25
+		String file = "Input3.txt";
+		Transaction testTransaction = new Transaction();
+		expectedInt = 4;
+		expectedItems = new ArrayList<String>(
+			Arrays.asList("1 imported bottle of perfume at 27.99", "1 bottle of perfume at 18.99", "1 packet of headache pills at 9.75", "1 box of imported chocolates at 11.25"));
+		if(testTransaction.scanItems(file).size() != expectedInt) {
+			System.out.println("Failed test: scanItems - number of items, "
+				+ "Expected: " + expectedInt + ", Actual: " + testTransaction.scanItems(file).size());
+		}
+		if(!testTransaction.scanItems(file).equals(expectedItems)) {
+			System.out.println("Failed test: scanItems - items, "
+				+ "Expected: " + expectedItems + ", Actual: " + testTransaction.scanItems(file));
+		}
+	}
+
+	public static void checkProduct () {
+		String expectedString;
+		BigDecimal expectedPrice;
+		// Product:
+		// 1 imported bottle of perfume: 54.65
+		Product testProduct = new Product("imported bottle of perfume", new BigDecimal("54.65"), false, true);
+		expectedString = "imported bottle of perfume";
+		if(!testProduct.name().equals(expectedString)) {
+			System.out.println("Failed test: getProductInfo - name, "
+				+ "Expected: " + expectedString + ", Actual: " + testProduct.name());
+		}
+		expectedPrice = new BigDecimal("54.65");
+		if(!testProduct.price().equals(expectedPrice)) {
+			System.out.println("Failed test: getProductInfo - price, "
+				+ "Expected: " + expectedPrice + ", Actual: " + testProduct.price());
+		}
 	}
 }
